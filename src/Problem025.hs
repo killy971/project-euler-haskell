6824a369ed40087971956a2499c9cff0ebdc5725
killy971 <killy971@gmail.com>
2013-09-21 23:56:37 +0900

    Added Haskell solution to Problem 25

---
+++ src/Problem025.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Util
+import Data.List
+
+digitsCount :: Show a => a -> Int
+digitsCount x = length $ show x
+
+hasThousandDigits :: Show a => a -> Bool
+hasThousandDigits x = digitsCount x == 1000
+
+solution :: Int
+solution = case findIndex hasThousandDigits fibs of
+	Just value -> value
+	Nothing    -> -1
+
+main :: IO ()
+main = do
+	print solution
