2d011666342e8d39cdd92029ac30b1868262a623
killy971 <killy971@gmail.com>
2013-09-22 17:32:36 +0900

    Added Haskell solution to problem 9

---
+++ src/Problem009.hs
@@ -0,0 +1,15 @@
+module Main where
+
+genericSolution :: Integer -> Integer -> Integer -> Integer -> Integer
+genericSolution a b c n =
+	if a * a + b * b == c * c
+		then a * b * c
+		else if c < b
+			then genericSolution (a + 1) (a + 2) (n - a - 3) n
+			else genericSolution a (b + 1) (n - a - b - 1) n
+
+solution :: Integer
+solution = genericSolution 1 2 997 1000
+
+main :: IO ()
+main = do print solution
