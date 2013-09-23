761663de96f5a81a225d0fb428896ef32c9ca38b
killy971 <killy971@gmail.com>
2013-09-23 15:34:07 +0900

    Fixed findIndexBy by introducing strict evaluation

--- src/Util.hs
+++ src/Util.hs
@@ -13,7 +13,7 @@ findIndexBy _ [] = error "Util.findIndexBy: empty list"
 findIndexBy f (x:xs) = findIndexBy' xs x 1 0
 	where 
 		findIndexBy' [] _ _ i = i
-		findIndexBy' (x:xs) y xi yi = if f x y
+		findIndexBy' (x:xs) y xi yi = xi `seq` if f x y
 			then findIndexBy' xs x (xi + 1) xi
 			else findIndexBy' xs y (xi + 1) yi
 
