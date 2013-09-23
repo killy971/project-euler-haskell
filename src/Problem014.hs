af278d588fa7fd85d37999b3a4902130254fae63
killy971 <killy971@gmail.com>
2013-09-23 14:11:35 +0900

    Added Haskell unoptimized solution to problem 14

---
+++ src/Problem014.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Util
+
+next :: Integer -> Integer
+next n = if odd n
+	then 3 * n + 1
+	else quot n 2
+
+collatzLength :: Integer -> Integer
+collatzLength start = collatzLength' start 2
+	where collatzLength' n result =
+		if n == 1
+			then result
+			else collatzLength' (next n) (result + 1)
+
+-- TODO fix findIndexBy terminal recursion
+solution :: Integer
+solution = 1 + findIndexBy (>) (map collatzLength [1..1000000])
+
+main :: IO ()
+main = do print solution
--- src/Util.hs
+++ src/Util.hs
@@ -8,6 +8,15 @@ fact n = product [1..n]
 fibs :: [Integer]
 fibs = 0 : scanl (+) 1 fibs
 
+findIndexBy :: (Ord a) => (a -> a -> Bool) -> [a] -> Integer
+findIndexBy _ [] = error "Util.findIndexBy: empty list"
+findIndexBy f (x:xs) = findIndexBy' xs x 1 0
+	where 
+		findIndexBy' [] _ _ i = i
+		findIndexBy' (x:xs) y xi yi = if f x y
+			then findIndexBy' xs x (xi + 1) xi
+			else findIndexBy' xs y (xi + 1) yi
+
 sortedPermutations :: Eq a => [a] -> [[a]]
 sortedPermutations [] = [[]]
 sortedPermutations xs = [ x:ys | x <- xs, ys <- sortedPermutations (delete x xs)]
