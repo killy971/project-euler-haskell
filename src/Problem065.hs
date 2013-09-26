d05ae1cce44d0ff479ca1f9cc09c6c279b618bf4
killy971 <killy971@gmail.com>
2013-09-27 08:16:59 +0900

    Added Haskell solution to problem 65

---
+++ src/Problem065.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.Digits
+import Data.Ratio
+import Util
+
+eCF :: [Integer]
+eCF = 2 : 1 : interleave3 (iterate (+ 2) 2) (repeat 1) (repeat 1)
+
+eConvergent :: Int -> [Integer]
+eConvergent = flip take eCF
+
+eFracAccFunc :: Ratio Integer -> Ratio Integer -> Ratio Integer
+eFracAccFunc = ratioAdd . (ratioDiv 1)
+
+eFrac :: Int -> Ratio Integer
+eFrac = foldl1 eFracAccFunc . map (% 1) . reverse . eConvergent
+
+genericSolution :: Int -> Integer
+genericSolution = sum . digits 10 . numerator . eFrac
+
+solution :: Integer
+solution = genericSolution 100
+
+main :: IO ()
+main = do print solution
--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,7 @@
 module Util where
 
 import Data.List (delete)
+import Data.Ratio
 
 ints :: (Enum t, Num t) => [t]
 ints = [1..]
@@ -8,11 +9,38 @@ ints = [1..]
 sq :: Integer -> Integer
 sq x = x * x
 
+ratioAdd :: Integral a => Ratio a -> Ratio a -> Ratio a
+ratioAdd x y =
+	let nx = numerator x in
+	let ny = numerator y in
+	let dx = denominator x in
+	let dy = denominator y in
+	(nx * dy + ny * dx) % (dx * dy)
+
+ratioDiv :: Integral a => Ratio a -> Ratio a -> Ratio a
+ratioDiv x y =
+	let nx = numerator x in
+	let ny = numerator y in
+	let dx = denominator x in
+	let dy = denominator y in
+	(nx * dy) % (dx * ny)
+
 fact :: (Enum a, Num a) => a -> a
 fact = product . enumFromTo 1
 
 fibs :: [Integer]
 fibs = 0 : scanl (+) 1 fibs
+
+interleave2 :: [a] -> [a] -> [a]
+interleave2 xs [] = xs
+interleave2 [] ys = ys
+interleave2 (x:xs) (y:ys) = x : y : interleave2 xs ys
+
+interleave3 :: [a] -> [a] -> [a] -> [a]
+interleave3 xs ys [] = interleave2 xs ys
+interleave3 xs [] zs = interleave2 xs zs
+interleave3 [] ys zs = interleave2 ys zs
+interleave3 (x:xs) (y:ys) (z:zs) = x : y : z : interleave3 xs ys zs
 
 findIndexBy :: (Ord a) => (a -> a -> Bool) -> [a] -> Integer
 findIndexBy _ [] = error "Util.findIndexBy: empty list"
