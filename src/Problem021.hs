34c125a375b4a1263e07a478f9aab8fc31fbfc1c
killy971 <killy971@gmail.com>
2013-09-28 20:31:36 +0900

    Added Haskell solution to problem 21

---
+++ src/Problem021.hs
@@ -0,0 +1,21 @@
+module Main where
+
+isDivisor :: Integer -> Integer -> Bool
+isDivisor x d = rem x d == 0
+
+properDivisors :: Integer -> [Integer]
+properDivisors x = filter (isDivisor x) [1..x `quot` 2]
+
+amicalNumber :: Integer -> Bool
+amicalNumber a = a /= b && a == sumB
+    where b = sum $ properDivisors a
+          sumB = sum $ properDivisors b
+
+genericSolution :: Integer -> Integer
+genericSolution n = sum $ filter amicalNumber [1..n]
+
+solution :: Integer
+solution = genericSolution 10000
+
+main :: IO ()
+main = do print solution
