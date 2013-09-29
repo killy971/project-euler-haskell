9a0cd9eeac37f3990fc613f6baf8d7b7eadab799
killy971 <killy971@gmail.com>
2013-09-29 18:28:16 +0900

    Added Haskell solution to problem 55

---
+++ src/Problem055.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Control.Monad
+import Data.Digits
+import Util
+
+reverseInteger :: Integer -> Integer
+reverseInteger = unDigits 10 . digitsRev 10
+
+isDecPalindrome :: Integer -> Bool
+isDecPalindrome = isPalindrome . digits 10
+
+isLychrel :: Integer -> Bool
+isLychrel = all (not . isDecPalindrome) . take 50 . tail . iterate next
+    where next = ap (+) reverseInteger
+
+genericSolution :: Integer -> Int
+genericSolution n = length $ filter isLychrel [1..n]
+
+solution :: Int
+solution = genericSolution 10000
+
+main :: IO ()
+main = do print solution
