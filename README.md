adac9cb41bf21f1c49ef92715642f6069512bc08
killy971 <killy971@gmail.com>
2013-10-01 23:01:37 +0900

    Added Haskell solution to problem 57

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ project-euler-haskell
 
 My Haskell solutions to Project Euler
 
-Haskell solutions for 1, 2, 9, 10, 12, 14, 21, 24, 25, 34, 36, 41, 52, 55, 65, 71, 72, 92
+Haskell solutions for 1, 2, 9, 10, 12, 14, 21, 24, 25, 34, 36, 41, 52, 55, 57, 65, 71, 72, 92
 
-Solutions for other problems implemented in different languages are availabe in the following repository: https://github.com/guillaume-nargeot/project-euler
+Solutions for other problems implemented in different languages are availabe in the following repository: http://github.com/guillaume-nargeot/project-euler
 
 <img src="http://projecteuler.net/profile/killy971.png"/>
---
+++ src/Problem057.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Data.Digits
+import Data.Ratio
+import Util
+
+next :: Ratio Integer -> Ratio Integer
+next = (+ 1) . (ratioDiv 1) . (+ 1)
+
+piExpansions :: Int -> [Ratio Integer]
+piExpansions = tail . flip take (iterate next 1)
+
+numeratorHasMoreDigitsThanDenominator :: Ratio Integer -> Bool
+numeratorHasMoreDigitsThanDenominator x =
+    (length $ digits 10 $ numerator x) > (length $ digits 10 $ denominator x)
+
+genericSolution :: Int -> Int
+genericSolution = length . filter numeratorHasMoreDigitsThanDenominator . piExpansions
+
+solution :: Int
+solution = genericSolution 1000
+
+main :: IO ()
+main = do print solution
