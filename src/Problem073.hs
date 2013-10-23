55525bace6abfc1af8e5cd28e32c86ce01d9cc87
killy971 <killy971@gmail.com>
2013-10-23 21:57:27 +0900

    Added Haskell solution to problem 73

---
+++ src/Problem073.hs
@@ -0,0 +1,29 @@
+module Main where
+
+startNumerator :: Integer -> Integer
+startNumerator d = if n > 3 * d
+    then n
+    else n + 1
+        where n = quot d 3
+
+endNumerator :: Integer -> Integer
+endNumerator d = if 2 * n < d
+   then n
+   else n - 1
+      where n = quot d 2
+
+countForDenominator :: Integer -> Integer
+countForDenominator d = countForDenominator' (startNumerator d) 0
+    where countForDenominator' n count = if n > endNum
+             then count
+             else countForDenominator' (n + 1) (if 1 == (gcd d n) then count + 1 else count)
+          endNum = endNumerator d
+
+genericSolution :: Integer -> Integer
+genericSolution = sum . map countForDenominator . enumFromTo 4
+
+solution :: Integer
+solution = genericSolution 12000
+
+main :: IO ()
+main = do print solution
