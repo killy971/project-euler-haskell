e46ba451482002ef74d6c5b3c3ced5c79ee5416d
killy971 <killy971@gmail.com>
2013-12-18 23:05:47 +0900

    Added Haskell solution to problem 38

---
+++ src/Problem038.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Data.Digits
+import Data.List
+
+isPandigital :: [Integer] -> Bool
+isPandigital = (== [1..9]) . sort
+
+applyProperty :: Integer -> [Integer]
+applyProperty = take 9 . concat . map (digits 10) . flip map [1..9] . (*)
+
+genericSolution :: [Integer] -> Integer
+genericSolution = unDigits 10 . last . filter isPandigital . map applyProperty
+
+solution :: Integer
+solution = genericSolution [1..10000]
+
+main :: IO ()
+main = do print solution
