0b58c29d95b6b3e933a4e5b3f9adced46834f3ef
killy971 <killy971@gmail.com>
2014-01-05 17:54:07 +0900

    Minor change to avoid warnings

---
+++ src/Problem128.hs
@@ -0,0 +1,10 @@
+module Main where
+
+genericSolution :: Integer -> Integer
+genericSolution = sum . filter multipleOf3or5 . enumFromTo 1 . subtract 1
+
+solution :: Integer
+solution = genericSolution 1000
+
+main :: IO ()
+main = do print solution
