d823c0bd0dd7ace29e124fa05a531fc3be494e26
killy971 <killy971@gmail.com>
2014-01-15 22:49:42 +0900

    Added Haskell solution for problem 33

---
+++ src/Problem033.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.Ratio
+
+reductibleFractions :: [Ratio Integer]
+reductibleFractions = [(10 * a + b) / (10 * b + c) | a <- [1..9], b <- [1..9], c <- [1..9], 9 * a * c + b * c == 10 * a * b]
+
+solution033 :: Integer
+solution033 = denominator $ product $ reductibleFractions
+
+main :: IO ()
+main = do print solution033
