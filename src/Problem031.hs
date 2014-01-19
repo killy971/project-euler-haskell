0bad0160687e3b7c7862992b33218524682b350c
killy971 <killy971@gmail.com>
2014-01-19 16:22:33 +0900

    Added Haskell solution to problem 31

---
+++ src/Problem031.hs
@@ -0,0 +1,14 @@
+module Main where
+
+combinations :: Integer -> [Integer] -> Integer
+combinations 0 _ = 1
+combinations _ [] = 0
+combinations r (c:cs) = if r < 0
+    then 0
+    else (combinations (r - c) (c:cs)) + (combinations r cs)
+
+solution031 :: Integer
+solution031 = combinations 200 [200, 100, 50, 20, 10, 5, 2, 1]
+
+main :: IO ()
+main = do print solution031
