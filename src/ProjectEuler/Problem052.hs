56f9c163b6f64af323cae1442de6379e28223588
killy971 <killy971@gmail.com>
2014-03-05 00:15:34 +0900

    Simplified code

--- src/ProjectEuler/Problem052.hs
+++ src/ProjectEuler/Problem052.hs
@@ -5,8 +5,7 @@ import Data.List
 import Util
 
 allEqual :: Eq a => [a] -> Bool
-allEqual [] = True
-allEqual (x:xs) = all (== x) xs
+allEqual xs = and $ zipWith (==) xs (tail xs)
 
 sameDigits :: Integer -> Bool
 sameDigits = allEqual . map (sort . digits 10) . flip map [2..6] . (*)
