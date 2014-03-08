e7ef57bc218e75993eba369ebab8d3631c7d1bc7
killy971 <killy971@gmail.com>
2014-03-08 18:26:06 +0900

    Fixed test that was broken on purpose for testing hpc-coveralls

--- test/TestUtil.hs
+++ test/TestUtil.hs
@@ -7,4 +7,4 @@ testUtil :: Test
 testUtil = "Util" ~: [
     map sq [0..4] ~=? [0,1,4,9,16],
 	take 8 fibs ~=? [0,1,1,2,3,5,8,13],
-	take 8 tribs ~=? [1,1,1,3,5,9,17,30]]
+	take 8 tribs ~=? [1,1,1,3,5,9,17,31]]
