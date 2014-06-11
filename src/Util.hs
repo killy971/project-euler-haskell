b23f57c9bd0e46745b9df8f6319c8da72909580d
Guillaume Nargeot <killy971+github@gmail.com>
2014-06-11 21:28:29 +0900

    Renamed findIndexBy function argument to clarify its role

--- src/Util.hs
+++ src/Util.hs
@@ -76,10 +76,10 @@ indexOfMax = fst . maximumBy (comparing snd) . zip [0..]
 
 findIndexBy :: (Ord a) => (a -> a -> Bool) -> [a] -> Integer
 findIndexBy _ [] = error "Util.findIndexBy: empty list"
-findIndexBy f (x:xs) = findIndexBy' xs x 1 0
+findIndexBy comp (x:xs) = findIndexBy' xs x 1 0
     where
         findIndexBy' [] _ _ i = i
-        findIndexBy' (y:ys) z yi zi = yi `seq` if f y z
+        findIndexBy' (y:ys) z yi zi = yi `seq` if comp y z
             then findIndexBy' ys y (yi + 1) yi
             else findIndexBy' ys z (yi + 1) zi
 
